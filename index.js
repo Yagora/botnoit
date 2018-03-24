8a09375f2dc42425ca5b5c2d48a113cc8fc64490
Tanguy Charon <tanguy.charon47@gmail.com>
2018-03-24 21:28:21 +0100

    [INIT] Init server

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); 
+
+app.post('/webhook', (req, res) => {
+
+  let body = req.body;
+
+  if (body.object === 'page') {
+
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
